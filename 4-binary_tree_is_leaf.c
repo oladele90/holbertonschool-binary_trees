cc18c448eb22c0a7c206f8dc3b3d4b2858b02e33
oladele90 <oladelemiro07@gmail.com>
2022-12-15 10:51:36 -0800

    fixed NULL passing

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,5 +10,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node->left == NULL && node->right == NULL)
 		return (1);
+	if (node == NULL || node->n == '\0')
+		return (0);
 	return (0);
 }
