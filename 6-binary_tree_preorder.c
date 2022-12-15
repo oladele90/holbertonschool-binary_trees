1a35bea98c9d815f77eb51622420352dbb1df888
oladele90 <oladelemiro07@gmail.com>
2022-12-15 11:29:30 -0800

    handle void func

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,13 +9,13 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree)
+	if (!tree || !func)
 		return;
 	if (tree)
 	{
 		func(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
+		return;
 	}
-	return;
 }
