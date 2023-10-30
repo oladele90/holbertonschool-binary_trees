25b4793afcfad61b37d22760dd2607aba8a7228d
oladele90 <oladelemiro07@gmail.com>
2023-10-30 21:34:19 +0000

    adds header file

--- binary_trees.h
+++ binary_trees.h
@@ -44,5 +44,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *set_tree(binary_tree_t *new, int value);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 #endif
