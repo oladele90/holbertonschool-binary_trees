6f792d11fc5bc8e1326fb7317a01f37f630af7cc
oladele90 <oladelemiro07@gmail.com>
2023-10-30 21:40:38 +0000

    adds NULL check for null tree being passed and fixes betty

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,17 +1,24 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_rotate_left - balances tree by rotating left
+* @tree: tree to balance
+* Return: pointer to tree
+*/
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *temp = NULL, *templ = NULL;
+	binary_tree_t *temp = NULL, *templ = NULL;
 
-    temp = tree;
-    tree = tree->right;
-    if (tree->left)
-        templ = tree->left;
-    else
-        templ = NULL;
-    tree->left = temp;
-    temp->parent = tree;
-    temp->right = templ;
-    return(tree);
+	if (!tree)
+		return (tree);
+	temp = tree;
+	tree = tree->right;
+	if (tree->left)
+		templ = tree->left;
+	else
+		templ = NULL;
+	tree->left = temp;
+	temp->parent = tree;
+	temp->right = templ;
+	return (tree);
 }
