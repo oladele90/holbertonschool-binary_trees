7d04aabf01883f581da75f41a47f21b435422fba
oladele90 <oladelemiro07@gmail.com>
2023-11-03 23:26:39 +0000

    balancer

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,7 +11,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int left_height, right_height;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
