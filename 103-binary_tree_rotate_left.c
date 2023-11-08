da316ff8bab5eb3aec1c160f409e80edd01e4570
oladele90 <oladelemiro07@gmail.com>
2023-11-08 22:13:06 +0000

    revises rotation

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -33,22 +33,23 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 */
 avl_t *avl_tree_rotate_left(avl_t *tree)
 {
-	avl_t *temp = NULL, *templ = NULL;
+	avl_t *old = NULL, *new = NULL;
 
-	if (!tree)
+	if (!tree || !tree->right)
 		return (tree);
-	temp = tree;
-	tree = tree->right;
-	if (tree->left)
-		templ = tree->left;
-	else
-		templ = NULL;
-	tree->left = temp;
-	temp->parent = tree;
-	temp->right = templ;
-	if (templ)
-		templ->parent = temp;
-	tree->parent = NULL;
-	return (tree);
+	old = tree;
+	new = tree->right;
+	old->parent = new;
+	if (!new->left)
+	{
+		new->left = old;
+		new->parent = NULL;
+		return (new);
+	}
+	old->right = new->left;
+	old->right->parent = old;
+	new->left = old;
+	new->parent = NULL;
+	return (new);
 }
 
--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -31,23 +31,24 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 * @tree: tree to balance
 * Return: pointer to tree
 */
-avl_t *avl_tree_rotate_right(avl_t *tree)
+avl_t *avl_tree_rotate_left(avl_t *tree)
 {
-	avl_t *temp = NULL, *tempr = NULL;
+	avl_t *old = NULL, *new = NULL;
 
-	if (!tree)
+	if (!tree || !tree->left)
 		return (tree);
-	temp = tree;
-	tree = tree->left;
-	if (tree->right)
-		tempr = tree->right;
-	else
-		tempr = NULL;
-	tree->right = temp;
-	temp->parent = tree;
-	temp->left = tempr;
-	if (tempr)
-		tempr->parent = temp;
-	tree->parent = NULL;
-	return (tree);
+	old = tree;
+	new = tree->left;
+	old->parent = new;
+	if (!new->right)
+	{
+		new->right = old;
+		new->parent = NULL;
+		return (new);
+	}
+	old->left = new->right;
+	old->left->parent = old;
+	new->right = old;
+	new->parent = NULL;
+	return (new);
 }
